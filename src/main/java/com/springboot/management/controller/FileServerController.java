22042b9814ce183431407d1cb7ad945882046ca3
Rascal <1799044809@qq.com>
2020-06-19 20:18:38 +0800

    fastdfs

---
+++ src/main/java/com/springboot/management/controller/FileServerController.java
@@ -0,0 +1,74 @@
+package com.springboot.management.controller;/*
+ *@Author lee
+ * @date 2020/06/18
+ */
+
+
+import com.springboot.management.vo.FileSystem;
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@CrossOrigin//允许跨域
+@RestController
+@RequestMapping("/filesystem")
+public class FileServerController {
+    @Value("${itcast-fastdfs.upload_location}")
+    private String upload_location;
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public FileSystem upload(@RequestParam("file") MultipartFile file) throws IOException {
+        //将文件先存储在web服务器上（本机），再调用fastDFS的client将文件上传到 fastDSF服务器
+        FileSystem fileSystem = new FileSystem();
+        //得到 文件的原始名称
+        String originalFilename = file.getOriginalFilename();
+        //扩展名
+        String extention = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String fileNameNew = UUID.randomUUID()+extention;
+        //定义file，使用file存储上传的文件
+        File file1 = new File(upload_location+fileNameNew);
+        file.transferTo(file1);
+        //获取新上传文件的物理路径
+        String newFilePath = file1.getAbsolutePath();
+        try {
+            //加载fastDFS客户端的配置 文件
+            ClientGlobal.initByProperties("config/fastdfs-client.properties");
+            System.out.println("network_timeout=" + ClientGlobal.g_network_timeout + "ms");
+            System.out.println("charset=" + ClientGlobal.g_charset);
+
+            //创建tracker的客户端
+            TrackerClient tracker = new TrackerClient();
+            TrackerServer trackerServer = tracker.getConnection();
+            StorageServer storageServer = null;
+            //定义storage的客户端
+            StorageClient1 client = new StorageClient1(trackerServer, storageServer);
+            //文件元信息
+            NameValuePair[] metaList = new NameValuePair[1];
+            metaList[0] = new NameValuePair("fileName", originalFilename);
+            //执行上传，将上传成功的存放在web服务器（本机）上的文件上传到 fastDFS
+            String fileId = client.upload_file1(newFilePath,null, metaList);
+            System.out.println("upload success. file id is: " + fileId);
+            fileSystem.setFileId(fileId);
+            fileSystem.setFilePath(fileId);
+            fileSystem.setFileName(originalFilename);
+
+            //通过调用service及dao将文件的路径存储到数据库中
+
+
+            //关闭trackerServer的连接
+            trackerServer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return fileSystem;
+    }
+
+}
---
+++ src/main/java/com/springboot/management/vo/FileSystem.java
@@ -0,0 +1,54 @@
+package com.springboot.management.vo;
+
+
+public class FileSystem {
+    private String fileId;
+
+    private String filePath;
+
+    private long fileSize;
+
+    private String fileName;
+
+    private String fileType;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+}
